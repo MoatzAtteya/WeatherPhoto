fa77dff53f96d689166b17e7252b2bbf56bf9307
ITWORXEDU\motaz.attya <motazattya@itworxedu.com>
2023-07-04 13:40:32 +0300

    - moving api_key to gradle

--- app/src/main/java/com/example/weatherphotos/DomainConstants.kt
+++ app/src/main/java/com/example/weatherphotos/DomainConstants.kt
@@ -2,10 +2,9 @@ package com.example.weatherphotos
 
 object DomainConstants {
 
-    private const val apiKey : String = "87fe72463e8772252a3bff691245c771"
     const val FACEBOOK_PACKAGE = "com.facebook.katana"
     const val TWITTER_PACKAGE = "com.twitter.android"
-    fun getApiKey() = apiKey
+    fun getApiKey() = BuildConfig.API_KEY
     fun getApiBaseUrl() = "https://api.openweathermap.org"
 
 }
